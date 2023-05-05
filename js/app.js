59465d7b17c2ad1efefb6bc419cddd0e2ac9df61
L <ice_rain2009@mail.ru>
2023-05-05 11:13:53 +0300

    add popApp for project

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
-$('a[data-target^="listLink"]').on('click', function () {
-    $('.navbar-collapse').collapse('hide');
+$(function() {
+    $('a[data-target^="listLink"]').on('click', function () {
+        $('.navbar-collapse').collapse('hide');
+    });
+var showAddPopup = function (){    
+    $('#addPopup').modal({
+        show:true
+    });
+};
+var initBtn = function (){
+    $ ('.show-info-popapp').click(showAddPopup);
+};
+initBtn();
 });
